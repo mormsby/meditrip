d96e423e6cd1682b4e8b5928518f6517ee5424d3
Matthew Ormsby <mormsby@medullan.com>
2014-11-12 21:11:54 -0400

    install generic template

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,11 @@ var app = angular.module('MTApp',
 ]);
 
 app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {
+      //controller : 'HomeController',
+      templateUrl : '/partials/home.html'
+    });
+
     $routeProvider.
       otherwise({
         redirectTo: '/'
