a1cb6653c303d915050ac76447c04e3890a55519
Sean Hoilett <sean@medullan.com>
2014-11-12 23:32:32 -0400

    Updated the server.js file

--- www/server.js
+++ www/server.js
@@ -1,49 +1,23 @@
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs")
-    port = process.argv[2] || 8888,
-    envKey = process.env.NODE_ENV || "local";
-    
+
+var envKey = process.env.NODE_ENV || "local";
 var env = {
 	local: {
-		port: 8888	
+		port: 8888
 	},
 	heroku: {
 		port: 80
-	}	
-}[envKey];
+	}
+}[envKey]; 
 
-console.log(env);
+var express = require('express');
 
-http.createServer(function(request, response) {
+var app = express();
+app.use('/', express.static(__dirname + '/'));
+app.get('/*', function(req, res) {
+	res.sendFile(__dirname + '/index.html')
+});
 
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
+var http = require("http");
+http.createServer(app).listen(env.port);
 
-  path.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
-
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
-
-      response.writeHead(200);
-      response.write(file, "binary");
-      response.end();
-    });
-  });
-}).listen(parseInt(env.port, 10));
-
-console.log("Static file server running at\n  => http://localhost:" + env.port + "/\nCTRL + C to shutdown");
+console.log("App running for the '%s' environment on port %s", envKey, env.port);
